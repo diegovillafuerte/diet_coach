c13f49a31cc8835d9a49eb8d430feb22954c6434
Diego Villafuerte <diegovillafuerte@Diegos-MacBook-Air.local>
2023-07-04 19:07:48 +0200

    few changes

--- modules/app.py
+++ modules/app.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import jwt
 from datetime import date, timedelta, datetime
 from flask import Flask, request, render_template, redirect, session, g, flash, jsonify, send_from_directory
@@ -13,7 +12,6 @@ app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
 CORS(app, supports_credentials=True)
-
 
 #APIS
 
@@ -39,7 +37,6 @@ def token_required(f):
 
     return decorated
 
-
 @app.route("/api/login", methods=["POST"])
 def api_login():
     data = request.get_json()
@@ -58,7 +55,6 @@ def api_login():
     except Exception as error:
         print(error)
         return jsonify({"error": "An error occurred during login"}), 500
-
 
 @app.route("/api/register", methods=["POST"])
 def api_register():
@@ -142,135 +138,6 @@ def get_meals_last_Ndays(current_user):
     except Exception as e:
         print(str(e))
         return jsonify({"error": "An error occurred while processing your request"}), 500
-
-
-
-
-
-#Regular functions
-
-@app.route("/", methods=["GET"])
-def index():
-    if 'email' not in session:
-        return redirect('/login')
-    return redirect('/welcome')
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
-        db_Session = DBSession()
-        user = db_Session.query(User).filter_by(email=email).first()        
-        if user and user.check_password(password):
-            session['email'] = email
-            return redirect('/welcome')
-        else:
-            return render_template('login.html', error="Invalid E-Mail or password")
-    else:
-        return render_template('login.html')
-    
-@app.route("/welcome", methods=["GET"])
-def welcome():
-    if 'email' not in session:
-        return redirect('/login')
-    else:
-        user_email = session['email']
-        todaysMeals = list_of_days_meals(user_email, date.today())
-        dailyTotal = get_daily_total(user_email, date.today())       
-        return render_template('welcome.html', email=user_email, today_meals = todaysMeals, dailyTotal=dailyTotal)
-
-@app.route("/get_info", methods=["POST"])
-def get_info():
-    food_description = request.form["food_description"]
-    nutritional_info = get_nutritional_info(food_description)
-    user_email = session['email']
-    todaysMeals = list_of_days_meals(user_email, date.today())
-    dailyTotal = get_daily_total(user_email, date.today()) 
-    if nutritional_info['calories'] == 'error':
-        flash("Error: Your query was not understood. Please try again.")
-        return render_template('welcome.html', today_meals = todaysMeals, dailyTotal=dailyTotal, error="Error: Your query was not understood. Please try again.")
-        
-    # Store the food description and nutritional info in session
-    session['food_description'] = food_description
-    session['nutritional_info'] = nutritional_info
-
-    return render_template('welcome.html',email=session['email'], nutritional_info=nutritional_info, food_description=food_description, today_meals = todaysMeals, dailyTotal=dailyTotal)
-
-
-@app.route("/add_meal", methods=["POST"])
-def add_meal():
-    # Retrieve the food description and nutritional info from session
-    food_description = session.get('food_description')
-    nutritional_info = session.get('nutritional_info')
-
-    # If the food description or nutritional info are not found in session, redirect to welcome page
-    if not food_description or not nutritional_info:
-        return redirect('/welcome')
-
-    # Add the new meal to the database
-    current_date = datetime.now().strftime("%Y-%m-%d")
-    meal = {
-        'meal': food_description,
-        'calories': nutritional_info['calories'],
-        'carbohydrates': nutritional_info['carbohydrates'],
-        'protein': nutritional_info['protein'],
-        'fat': nutritional_info['fat'],
-        'sodium': nutritional_info['sodium'],
-        'explanation': nutritional_info['explanation'],
-        'date': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        'user_email': session['email']
-    }
-    add_meals(meal)
-
-    # Clear the food description and nutritional info from session
-    session.pop('food_description', None)
-    session.pop('nutritional_info', None)
-
-    # Get todays meal summary
-    user_email = session['email']
-    todaysMeals = list_of_days_meals(user_email, date.today())
-    dailyTotal = get_daily_total(user_email, date.today()) 
-
-    # Redirect to welcome page
-    return render_template('welcome.html', today_meals = todaysMeals, dailyTotal=dailyTotal)
-
-@app.route("/deleteMeal/<int:meal_id>", methods=["GET"])
-def delete_Meal(meal_id):
-    """
-    Deletes the meal with the specified ID from the database.
-
-    Args:
-        meal_id: ID of the meal to delete.
-
-    Returns:
-        A redirect to the home page.
-    """
-    try:
-        delete_meals(meal_id)
-    except ValueError as e:
-        print(e) 
-    user_email = session['email']
-    todaysMeals = list_of_days_meals(user_email, date.today())
-    dailyTotal = get_daily_total(user_email, date.today())       
-    return render_template('welcome.html', email=user_email, today_meals = todaysMeals, dailyTotal=dailyTotal)
-
-
-
-    
-@app.route("/logout", methods=["GET"])
-def logout():
-    session.clear()
-    return redirect('/login')
-
-@app.after_request
-def add_vary_cookie_header(response):
-    response.headers['Vary'] = 'Cookie'
-    return response
-
-api_key = os.getenv("OPENAI_API_KEY")
-if api_key is None:
-    sys.exit("Error: OPENAI_API_KEY environment variable not set.")
 
 if __name__ == "__main__":
     app.run(debug=True)
